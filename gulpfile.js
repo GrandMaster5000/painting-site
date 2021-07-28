479192bd77139f66b9e35d3a912d962402f974a2
Tolik22869 <ggamer556@mail.ru>
2021-07-28 11:00:33 +0300

    start work

---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+"use strict";
+
+const gulp = require("gulp");
+const webpack = require("webpack-stream");
+const browsersync = require("browser-sync");
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const cssmin = require('gulp-cssmin');
+const concatCss = require('gulp-concat-css');
+
+const dist = "./dist/";
+
+gulp.task('html', function() {
+  return gulp.src("src/*.html")
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('./src/assets/css/*.css')
+    .pipe(cssmin())
+    .pipe(gulp.dest('dist/assets/css/'));
+ });
+
+gulp.task("build-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'development',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    watch: false,
+                    devtool: "source-map",
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: true,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest(dist))
+                .on("end", browsersync.reload);
+});
+
+
+gulp.task("watch", () => {
+    browsersync.init({
+		server: "./dist/",
+		port: 4000,
+		notify: true
+    });
+    
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("./src/assets/fonts/*.*", gulp.parallel("fonts"));
+    gulp.watch("./src/assets/img/*.*", gulp.parallel("images"));
+    gulp.watch("./src/assets/css/*.*", gulp.parallel("styles"));
+    gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+});
+
+gulp.task('server', function() {
+  return gulp.src("src/assets/server.php")
+      .pipe(gulp.dest("dist/assets"))
+      .pipe(browsersync.stream());
+});
+
+gulp.task('fonts', function() {
+  return gulp.src("src/assets/fonts/**/*")
+      .pipe(gulp.dest("dist/assets/fonts"))
+      .pipe(browsersync.stream());
+});
+
+gulp.task('images', function() {
+  return gulp.src("src/assets/img/**/*")
+      .pipe(imagemin())
+      .pipe(gulp.dest("dist/assets/img"))
+      .pipe(browsersync.stream());
+});
+
+gulp.task("build", gulp.parallel("html",  "build-js", 'fonts', 'images', 'styles', 'server'));
+
+gulp.task("build-prod-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'production',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest(dist));
+});
+
+gulp.task("default", gulp.parallel("watch", "build"));
